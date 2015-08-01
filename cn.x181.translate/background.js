96143d5c0c8af1ab1a8be48ad9b79b6457aa2cd8
redky <redky@qq.com>
2015-08-01 21:08:21 +0800

    Add Translate extension

---
+++ cn.x181.translate/background.js
@@ -0,0 +1,64 @@
+;(function() {
+  // chrome tools.
+  // https://chrome.google.com/webstore/detail/fjccknnhdnkbanjilpjddjhmkghmachn
+
+  chrome = typeof chrome === 'undefined' ? sogouExplorer : chrome;
+
+  function callback(clickData, tab) {
+    var id = clickData.menuItemId;
+    if (menuMap[id]) {
+      var key = menuMap[id].key;
+      var text = clickData[key];
+      chrome.tabs.create({
+        // http://zh.wikipedia.org/wiki/
+        url: menuMap[id].url.replace(/\{keyword\}/ig, encodeURIComponent(text)),
+        active: true
+      });
+    }
+  }
+
+  var contexts = {
+    "translate": {
+      context: 'selection',
+      title: '使用百度翻译翻译',
+      key: 'selectionText',
+      url: 'http://dict.baidu.com/s?wd={keyword}'
+    }
+  };
+
+  var menuMap = {};
+
+  function bindContextMenu() {
+    var curContext;
+    var context;
+    var title;
+    var id;
+    var contextIDIndex = 1;
+    for ( var key in contexts ) {
+      if ( contexts.hasOwnProperty(key) ) {
+        curContext = contexts[key];
+        title = curContext.title;
+        context = curContext.context;
+        id = chrome.contextMenus.create({
+          title: title,
+          contexts: [ context ],
+          // event page 不支持 onclick 参数.
+          // 需要使用 chrome.contextMenus.onClick 绑定.
+          onclick: callback
+        });
+        menuMap[id] = curContext;
+      }
+    }
+  }
+
+  if ( chrome.runtime && chrome.onInstalled ) {
+    // 安装时, 生成右键菜单..
+    chrome.runtime.onInstalled.addListener(function( details ) {
+      bindContextMenu();
+    });
+  }
+  else {
+    bindContextMenu();
+  }
+})();
+
