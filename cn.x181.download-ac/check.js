72f1256833c82be82a3eca09d8b0309cf458050e
redky <redky@qq.com>
2015-08-01 22:51:48 +0800

    checked by filesize

--- cn.x181.download-ac/check.js
+++ cn.x181.download-ac/check.js
@@ -24,29 +24,65 @@ var done = [];
 var fail = [];
 var fs = require('fs');
 var path = require('path');
-var base = 'd:\\download';
+var base = './do';
 function check(base) {
   fs.readdir(base, function(error, files) {
-    if (error) return console.log(base);
+    if (error) {
+      return console.log('Read base path error!');
+    }
     queue(files, function(file, index, next) {
-      var _file = path.resolve(base, file);
-      fs.stat(_file, function(error, stat) {
-        if (error) return console.log('!!stat:', _file);
-        if (stat.isDirectory()) {
-          fs.readdir(_file, function(error, files) {
-            if (error) return console.log('error:', _file);
-            if (files.length < 12) {
-              fail.push(files.length + '    ' + _file);
-            } else {
-              done.push(files.length + '    ' + _file);
+      var folder = path.resolve(base, file);
+      fs.stat(folder, function(error, stat) {
+        if (error) {
+          console.log('Read Directory error!:', folder);
+          return next();
+        }
+        var is = stat.isDirectory();
+        if (!is) {
+          console.log('only read Directory.', folder);
+          return next();
+        } else {
+          fs.readdir(folder, function(error, files) {
+            if (error) {
+              console.log('error:', folder);
+              return next();
             }
-            next();
-          })
+            if (files.length < 12) {
+              fail.push(folder + '  < 12' + files.length);
+              return next();
+            } else {
+              // fixed sort!
+              files.sort(function(a, b) {
+                var x = parseInt(a), y = parseInt(b);
+                if (isNaN(x)) { x = -1; }
+                if (isNaN(y)) { y = -1; }
+                return x > y ? 1 : -1;
+              });
+              var l = files.length;
+              queue(files, function(file, index, next) {
+                var f = path.resolve(folder, file);
+                fs.stat(f, function(error, stat) {
+                  if (error) {
+                    console.log('file state error.', f);
+                    return next();
+                  }
+                  if (stat.size > 60 * 1024) {
+                    done.push(f + ' ' + stat.size);
+                  } else if (index + 2 < l && index > 2) {
+                    fail.push(f + ' file size error ' + stat.size);
+                  }
+                  next();
+                });
+              }, function() {
+                next();
+              });
+            }
+          });
         }        
       });
     }, function() {
-      var _fail = path.resolve('d:\\error.txt');
-      var _done = path.resolve('d:\\done.txt');
+      var _fail = path.resolve('./error.txt');
+      var _done = path.resolve('./done.txt');
       fs.writeFileSync(_fail, fail.join('\n'), {encoding: 'utf8'});
       fs.writeFileSync(_done, done.join('\n'), {encoding: 'utf8'});
     });
