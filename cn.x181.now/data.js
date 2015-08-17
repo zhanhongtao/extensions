6aff5e6a114040367dc5ff2822947a9d03e9994b
redky <redky@qq.com>
2015-08-17 17:39:57 +0800

    把 http 升级为 https

--- cn.x181.now/data.js
+++ cn.x181.now/data.js
@@ -1,5 +1,5 @@
 var urls = {
-  'sogou': 'http://www.sogou.com/sie?ie=utf8&query={keyword}',
+  'sogou': 'https://www.sogou.com/sie?ie=utf8&query={keyword}',
   'google': 'https://www.google.com.hk/?gws_rd=cr,ssl#safe=strict&q={keyword}'
 };
 
